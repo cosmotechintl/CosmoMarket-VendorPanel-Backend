4dccf8d25c3aa8c73977d0bd0f906de9c8ce9ce5
Manjul Tamang <manzultamang777@gmail.com>
2024-06-24 13:18:46 +0545

    refactored

--- microservices/products-service/src/main/java/com/cosmo/productsservice/ProductsServiceApplication.java
+++ microservices/products-service/src/main/java/com/cosmo/productsservice/ProductsServiceApplication.java
@@ -2,7 +2,16 @@ package com.cosmo.productsservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
+@EntityScan(basePackages = {"com.cosmo"})
+@EnableJpaRepositories(basePackages = {"com.cosmo"})
+@ComponentScan(basePackages = {"com.cosmo"})
+@EnableWebMvc
 @SpringBootApplication(scanBasePackages = "com.cosmo")
 public class ProductsServiceApplication {
 
