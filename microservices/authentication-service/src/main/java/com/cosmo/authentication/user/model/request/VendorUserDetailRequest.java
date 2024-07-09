206a52884263c2e22de31b2f3d3acb7e9b53dae5
Manjul Tamang <manzultamang777@gmail.com>
2024-07-09 12:57:23 +0545

    code refactored in vendor details

--- microservices/authentication-service/src/main/java/com/cosmo/authentication/user/model/request/VendorUserDetailRequest.java
+++ microservices/authentication-service/src/main/java/com/cosmo/authentication/user/model/request/VendorUserDetailRequest.java
@@ -1,5 +1,6 @@
 package com.cosmo.authentication.user.model.request;
 
+import com.cosmo.authentication.accessGroup.model.AccessGroupDto;
 import com.cosmo.common.model.ModelBase;
 import lombok.Getter;
 import lombok.Setter;
@@ -9,4 +10,6 @@ import lombok.Setter;
 public class VendorUserDetailRequest extends ModelBase {
     private String name;
     private String email;
+    private String mobileNumber;
+    private AccessGroupDto accessGroup;
 }
