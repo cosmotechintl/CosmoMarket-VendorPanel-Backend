a05f320fc23bc9d0dbe8073361b415385591be39
AmritkThapa <amritthapa.6024@gmail.com>
2024-07-26 18:27:42 +0545

    Logging Done

---
+++ microservices/vendor-service/src/main/java/com/cosmo/vendorservice/filter/RequestResponseLogger.java
@@ -0,0 +1,21 @@
+package com.cosmo.vendorservice.filter;
+
+import jakarta.servlet.*;
+import jakarta.servlet.http.HttpServletRequest;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+
+@Slf4j
+@Component
+public class RequestResponseLogger implements Filter {
+    @Override
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+        HttpServletRequest httpServletRequest = (HttpServletRequest) servletRequest;
+        log.info("URI: {}", httpServletRequest.getRequestURI());
+
+        filterChain.doFilter(servletRequest,servletResponse);
+
+    }
+}
