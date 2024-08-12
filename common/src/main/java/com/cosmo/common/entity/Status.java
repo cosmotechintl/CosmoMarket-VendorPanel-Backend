459c48306b187a7bbad149b18e94aa3db8060f5c
AmritkThapa <amritthapa.6024@gmail.com>
2024-08-12 17:02:21 +0545

    2nd Level Hibernate cache using EHCache

--- common/src/main/java/com/cosmo/common/entity/Status.java
+++ common/src/main/java/com/cosmo/common/entity/Status.java
@@ -1,16 +1,21 @@
 package com.cosmo.common.entity;
 
 import com.cosmo.common.abstractEntity.AbstractEntity;
+import jakarta.persistence.Cacheable;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Table;
 import lombok.Getter;
 import lombok.Setter;
+import org.hibernate.annotations.Cache;
+import org.hibernate.annotations.CacheConcurrencyStrategy;
 
 @Getter
 @Setter
 @Entity
 @Table(name = "status")
+@Cacheable
+@Cache(usage = CacheConcurrencyStrategy.READ_ONLY)
 public class Status extends AbstractEntity {
 
     @Column(name = "name", nullable = false)
